ce293441049ae45746a5fb93e46db88d268a8613
Dani <slimshady3753@protonmail.com>
2025-03-04 00:38:40 +0330

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const newMessageRouter = require('./routs/newMessageRouter');
 const app = express();
 const assetsPath = path.join(__dirname,"public");
 app.use(express.static(assetsPath))
-
+const port = 3000;
 app.use(express.urlencoded({extended: true}))
 
 app.set('view engine','ejs')
@@ -14,4 +14,4 @@ app.set('view engine','ejs')
 app.use('/',indexRouter);
 app.use('/new',newMessageRouter);
 
-app.listen(3000,(err)=>console.log(err))
+app.listen(port,(err)=>console.log(err))
